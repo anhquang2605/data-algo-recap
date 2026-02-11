3b5ba96629dbb3341853a613b531d782bbef66ac
Anh Quang Chu <anhquang2605@gmail.com>
2026-02-10 18:59:57 -0800

    added base

---
+++ library.ts
@@ -0,0 +1,22 @@
+export const selectionSort = (arr: number[]): number[] => {
+    const len = arr.length;
+    
+    for (let i = 1; i < len - 1; i++) {
+        let temp: number = arr[i];
+        let left: number = i - 1;
+        while(left >= 0) {
+            
+            if(temp < arr[left]) {
+                //move the value at left to the right
+                arr[left + 1] = arr[left];
+                left--;
+            } else {
+                break;
+            }
+        }
+
+    }
+    return arr;
+}
+
+//when gap is 0 or when the value compared is less than the value at the temp, end phase and insert the temp value at the correct position.
