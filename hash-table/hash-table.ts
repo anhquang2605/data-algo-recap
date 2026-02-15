dc78b929886b0398d34f0bebc084dc0c589a09a4
Anh Quang Chu <anhquang2605@gmail.com>
2026-02-15 11:12:55 -0800

    addedm ore code

--- hash-table/hash-table.ts
+++ hash-table/hash-table.ts
@@ -1,9 +1,21 @@
-export const hashTable = (arr: number[]) => {
+//this function take any arr and turn into a hash table
+export const hashTable = (arr: any[]) => {
     const hashTable: number[] = new Array(arr.length).fill(0);
     for (let i = 0; i < arr.length; i++) {
         hashTable[arr[i]] = 1;
     }
     return hashTable;
+}
+
+export const firstNonDuplicate = (arr: any[]) => {
+    //create a hash table to store the value of the arr
+    const table = hashTable(arr);
+    for (let i = 0; i < arr.length; i++) {
+        if(table[arr[i]] === 1) {
+
+        }
+    }
+
 }
 
 
