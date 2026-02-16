4308e8490b3e2da6c19b77e64632b62a05daac69
Anh Quang Chu <anhquang2605@gmail.com>
2026-02-16 13:58:10 -0800

    added stack revsered

--- queue/queue.js
+++ queue/queue.js
@@ -1,4 +1,47 @@
-import { Stack } from "../stack/stack.js";
+//define stack class
+class Stack {
+    constructor() {
+        this.stack = [];
+    }
+    push(value) {
+        this.stack.push(value);
+    }
+    pop() {
+        if(this.stack.length === 0) {
+            return null;
+        }
+        return this.stack.pop();
+    }
+    read () {
+        if(this.stack.length === 0) {
+            return null;
+        }
+        return this.stack[this.stack.length - 1];
+    }
+
+    size () {
+        return this.stack.length;
+    }
+
+    isEmpty () {
+        return this.stack.length === 0;
+    }
+
+    clear () {
+        this.stack = [];
+    }
+
+    print () {
+        console.log(this.stack);
+    }
+    
+    createStackFromArr (arr) {
+        for (let i = 0; i < arr.length; i++) {
+            this.push(arr[i]);
+        }
+    }
+}
+
 class Queue {
     constructor() {
         this.queue = [];
