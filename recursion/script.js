696c01d8868014850fdf7f974fbc5a326b900681
Anh Quang Chu <anhquang2605@gmail.com>
2026-02-17 17:15:51 -0800

    added factorial

--- recursion/script.js
+++ recursion/script.js
@@ -6,5 +6,12 @@ const countdown = (num) => {
         countdown(num - 1);
     }
 }
+const factorial = (num) => {
+    if(num === 1){
+        return num;
+    } else {
+        return num * factorial(num - 1);
+    }
+}
 
 countdown(10);
