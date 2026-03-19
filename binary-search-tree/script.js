9b1eb6ef9775757df5b93f2376937234608e40e2
anhquang2605 <anhquang2605@gmail.com>
2026-03-18 17:30:44 -0700

    added search for BST

--- binary-search-tree/script.js
+++ binary-search-tree/script.js
@@ -1,7 +1,21 @@
-class BST {
+class TreeNode {
     constructor(value, left = null, right = null) {
         this.value = value;
         this.left = left;
         this.right = right;
+    }
+}
+class BST {
+    constructor() {
+        this.root = null;
+    }
+    search() {
+        currentNode = this.root;
+        while(currentNode !== null){
+            if(currentNode.value === value) return currentNode;
+            else if (currentNode.value > value) currentNode = currentNode.left;
+            else currentNode = currentNode.right;
+        }
+        return null;
     }
 }
