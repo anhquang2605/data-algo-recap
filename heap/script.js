824e1c902d31ca73da34511a61f72b7dea0b9ccc
anhquang2605 <anhquang2605@gmail.com>
2026-03-26 16:35:09 -0700

    added way to find parent and children

--- heap/script.js
+++ heap/script.js
@@ -8,4 +8,13 @@ class Heap {
     last()  {
         return this.heap[this.heap.length - 1];
     }
+    leftChildIndex(index) {
+        return 2 * index + 1;
+    }
+    rightChildIndex(index) {
+        return 2 * index + 2;
+    }
+    parentIndex(index) {
+        return Math.floor((index - 1) / 2);
+    }
 }
