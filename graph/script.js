25702391527c8f0214b05ef5eecbc8b4bada2609
Anh Quang Chu <anhquang2605@gmail.com>
2026-04-03 11:03:12 -0700

    added another DFS version

--- graph/script.js
+++ graph/script.js
@@ -13,12 +13,28 @@ class Vertex{
         if(visited.has(vertex.value)){
             return;
         }
+        visited.add(vertex.value);
         for(let adjacent of vertex.adjacencyList){
             if(!visited.has(adjacent.value)){
                 visited.add(adjacent.value);
                 this.DFS(adjacent, visited);
             } 
         }
-
+    }
+    DFSValue(value, vertex, visited = new Set(),){
+        if(vertex.value === value){
+            return vertex;
+        }
+        visited.add(vertex.value);
+        for(let adjacent of vertex.adjacencyList){
+            if(!visited.has(adjacent.value)){
+                visited.add(adjacent.value);
+                let found = this.DFSValue(value, adjacent, visited);
+                if(found){
+                    return found;
+                }
+            }
+        }
+        return null;
     }
 }
