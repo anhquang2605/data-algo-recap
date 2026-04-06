f0542fab34d488591997e257ea636469fcf09ebe
anhquang2605 <anhquang2605@gmail.com>
2026-04-06 14:01:52 -0700

    added note

--- graph/script.js
+++ graph/script.js
@@ -1,3 +1,18 @@
+class Queue{
+    constructor(){
+        this.items = [];
+    }
+    enqueue(item){
+        this.items.push(item);
+    }
+    dequeue(){
+        return this.items.shift();
+    }
+    peek(){ 
+        return this.items[0];
+    }
+
+}
 class Vertex{
     constructor(value){
         this.value = value;
@@ -9,6 +24,7 @@ class Vertex{
         } 
         vertex.addAdjacent(this);
     }
+    
     DFS(vertex, visited = new Set()){
         if(visited.has(vertex.value)){
             return;
@@ -19,6 +35,22 @@ class Vertex{
                 visited.add(adjacent.value);
                 this.DFS(adjacent, visited);
             } 
+        }
+    }
+    //to visist all the adjacent vertices first of current vertex
+    BFS(vertex){
+        let visited = new Set();
+        let queue = new Queue();
+        queue.enqueue(vertex);
+        visited.add(vertex.value);
+        while(queue.items.length > 0){
+            let current = queue.dequeue();
+            for(let adjacent of current.adjacencyList){
+                if(!visited.has(adjacent.value)){
+                    visited.add(adjacent.value);
+                    queue.enqueue(adjacent);
+                }
+            }
         }
     }
     DFSValue(value, vertex, visited = new Set(),){
