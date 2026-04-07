215a9321c27b12bca14a9613f5b73dd66d3354c6
anhquang2605 <anhquang2605@gmail.com>
2026-04-07 11:11:51 -0700

    finished the first part

--- graph/script2.js
+++ graph/script2.js
@@ -12,6 +12,28 @@ class WeightGraphVertex{
 const djikstra = (start, end) => {
     const cheapeestPriceTable = {}; //storing the cheapest price to get from start to each vertex and beyond
     const cheapestPreviousVertexTable = {};//storing the previous vertex for each cheapest price
+    const visitedVertices = new Set();
+    const unvisitedVertices = [];
+    unvisitedVertices.push(start.value);
+    cheapeestPriceTable[start.value] = 0;
+    let currentVertex = start;
+    while(currentVertex){
+        visitedVertices.add(currentVertex.value);
+        unvisitedVertices.delete(currentVertex.value);
+        const adjacentVertices = currentVertex.adjacencyList;
+        for(let adjacentVertex in adjacentVertices){
+            let {vertex, weight} = adjacentVertices[adjacentVertex];
+            if(!visitedVertices.has(vertex.value)){
+                unvisitedVertices.push(vertex);
+                const priceToAdjacentVertex = cheapeestPriceTable[currentVertex.value] + weight;
+                if(!cheapeestPriceTable[vertex] || priceToAdjacentVertex < cheapeestPriceTable[vertex]){
+                    cheapeestPriceTable[vertex] = priceToAdjacentVertex;
+                    cheapestPreviousVertexTable[vertex] = currentVertex.value;
+                }               
+            }
+        }
+    }
+
 }
 const alanta = new WeightGraphVertex('Atlanta');
 const boston = new WeightGraphVertex('Boston');
