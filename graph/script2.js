7c1a74e9d16f015c5003c9d7fc5138d202a064b3
anhquang2605 <anhquang2605@gmail.com>
2026-04-07 10:47:13 -0700

    setup

--- graph/script2.js
+++ graph/script2.js
@@ -4,8 +4,22 @@ class WeightGraphVertex{
         this.adjacencyList  = {};
     }
     addAdjacent(vertex, weight){
-        if(!this.adjacencyList[vertex]){
-            this.adjacencyList[vertex] = weight;
+        if(!this.adjacencyList[vertex.value]){
+            this.adjacencyList[vertex.value] = weight;
         }
     }
 }
+const alanta = new WeightGraphVertex('Atlanta');
+const boston = new WeightGraphVertex('Boston');
+const chicago = new WeightGraphVertex('Chicago');
+const denver = new WeightGraphVertex('Denver');
+const elPaso = new WeightGraphVertex('El Paso');
+
+alanta.addAdjacent(boston, 100);
+alanta.addAdjacent(chicago, 160);
+boston.addAdjacent(chicago, 120);
+boston.addAdjacent(denver, 180);
+chicago.addAdjacent(elPaso, 80);
+denver.addAdjacent(chicago, 40);
+denver.addAdjacent(elPaso, 140);
+elPaso.addAdjacent(boston, 100);
