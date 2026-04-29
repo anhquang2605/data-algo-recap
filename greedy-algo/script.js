aff3c39af8b32d729c0618f026aa8a19d015d1de
Anh Quang Chu <anhquang2605@gmail.com>
2026-04-28 17:06:19 -0700

    added

--- greedy-algo/script.js
+++ greedy-algo/script.js
@@ -145,6 +145,7 @@ const findGreatestProductOptimized = (arr) => {
 const sortTempReading = (temps) => {
     let tempStore = new Array(20);
     let maxDiff = 0;
+    let result = [];
     for (let i = 0; i < temps.length; i++) {
         let current = temps[i];
         let diff = current - 97;
@@ -158,7 +159,17 @@ const sortTempReading = (temps) => {
         }
         
     }
+    for(let i = 0; i <= maxDiff; i += 1){
+        let currentCount = tempStore[maxDiff];
+        if(currentCount){
+            for (let j = 0; j < currentCount; j += 1){
+                result.push(97 + i);
+            }
+        }
+    }
+    return result;
 }
 const arr4 = [1, 4, -5, -8 , 2];
 
 const temp = [97.0, 99.0, 98.0 , 99.1, 98.9, 97.9, 98.1, 97.2]; //range from 97.0 to 99.0
+
